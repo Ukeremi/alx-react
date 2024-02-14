f8d1cadf9c944809bb57c931881e56ad364b5270
Ukpono Umoren <ukpono9@gmail.com>
2024-02-14 00:59:10 -0800

    updating index

--- 0x09-react_redux_connectors_and_providers/task_3/dashboard/src/index.js
+++ 0x09-react_redux_connectors_and_providers/task_3/dashboard/src/index.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App/App';
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import { uiReducer } from './reducers/uiReducers';
+import thunk from 'redux-thunk';
+
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+export const store = createStore(
+  uiReducer,
+  composeEnhancers(applyMiddleware(thunk))
+)
 
 ReactDOM.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
-	document.getElementById('root')
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
